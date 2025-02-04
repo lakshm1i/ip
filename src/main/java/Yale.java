16c67f76d3204c6c3c104072c59c418584617b55
lakshm1i <>
2025-02-05 01:24:09 +0800

    Updated code according to instructions
    Upgraded echo method to list the task or add the task depending on input. addTask will add input into array of size 100 and increase size of array accordingly. listTask will list down task number and task name.

--- src/main/java/Yale.java
+++ src/main/java/Yale.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Yale {
+    private static String[] tasks = new String[100]; //Array to store tasks
+    private static int taskCounter = 0; //Track number of tasks stored
+
     public static void main(String[] args) {
         greetUser();
         echo();
@@ -20,9 +23,29 @@ public class Yale {
         Scanner in = new Scanner(System.in);
         line = in.nextLine();  // Read the first line of user input
         while (!line.equalsIgnoreCase("bye")) {
-            System.out.println(line); // Echo the user's input
+            if (line.equalsIgnoreCase("list")) {
+                listTask(); //Display all the tasks
+            }
+            else {
+                addTask(line); //Add the task to the list
+            }
             line = in.nextLine(); // Read the next line of user input
         }
         exitChat(); // Exit the chat when the user types "bye"
+    }
+
+    private static void addTask(String task) {
+        tasks[taskCounter] = task; //Add task to array
+        taskCounter++; //Increment the task counter
+        System.out.println("added: " + task);
+    }
+
+    private static void listTask() {
+        if (taskCounter == 0) { //Check if there are no tasks in array
+            System.out.println("No tasks have been added");
+        }
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.println((i+1) + ". " + tasks[i]); //Print task number and name
+        }
     }
 }
