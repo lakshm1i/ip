2e0a4641335b5494f980f4c4479825b34b6bad2a
lakshm1i <>
2025-02-04 21:52:42 +0800

    Started on iP and did level 0
    I renamed the chatbot from Duke to Yale. I created two methods: greetUser to greet the user and exitChat to display the exit message.

---
+++ src/main/java/Yale.java
@@ -0,0 +1,14 @@
+public class Yale {
+    public static void main(String[] args) {
+        greetUser();
+        exitChat();
+    }
+    public static void greetUser() {
+        System.out.println("Hello! I'm Yale");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exitChat() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
