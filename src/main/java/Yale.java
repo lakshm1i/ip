d05cae96689675ac4f4f0b38b97559fa11a241f9
lakshm1i <>
2025-02-05 00:19:50 +0800

    Updated code according to instructions.
    Created a echo method. It uses scanner to read a line of input from the keyboard and if it is not bye (ignoring case), it will echo back the input. If input is bye, will exitchat.

--- src/main/java/Yale.java
+++ src/main/java/Yale.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Yale {
     public static void main(String[] args) {
         greetUser();
-        exitChat();
+        echo();
     }
+
     public static void greetUser() {
         System.out.println("Hello! I'm Yale");
         System.out.println("What can I do for you?");
@@ -10,5 +13,16 @@ public class Yale {
 
     public static void exitChat() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();  // Read the first line of user input
+        while (!line.equalsIgnoreCase("bye")) {
+            System.out.println(line); // Echo the user's input
+            line = in.nextLine(); // Read the next line of user input
+        }
+        exitChat(); // Exit the chat when the user types "bye"
     }
 }
