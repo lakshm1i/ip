7e99d9b400eb70927b2a258e1833a378506aea91
lakshm1i <e1155664@u.nus.edu>
2025-02-23 14:53:45 +0800

    level-6 save

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -4,13 +4,21 @@ public class TaskManager {
 
     private static final int MAX_TASKS = 100;
     private final ArrayList<Task> taskList = new ArrayList<>();
+    private static final String FILE_PATH = "./data/Yale.txt";
+    private Storage storage;
 
+    public TaskManager() {
+        storage = new Storage(FILE_PATH); // Initialize the Storage with file path
+        loadTasks(); // Load tasks from file
+    }
 
     public void addTask(Task task) {
-        taskList.add(task); // Add task to the list
+        taskList.add(task);  // Add task to the list
         System.out.println("Got it. I've added this task:");
         System.out.println(" " + task);
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+        saveTasks();
+
     }
 
     public void displayTaskList() {
@@ -29,6 +37,7 @@ public class TaskManager {
             taskList.get(taskIndex-1).markAsDone();
             System.out.println("Nice! I've marked this task as done:");
             System.out.println("  " + taskList.get(taskIndex-1));
+            saveTasks();
         } else {
             System.out.println("Invalid task index");
         }
@@ -39,21 +48,21 @@ public class TaskManager {
             taskList.get(index-1).markAsNotDone();
             System.out.println("OK, I've marked this task as not done yet:");
             System.out.println("  " + taskList.get(index-1));
+            saveTasks();
         }
         else {
             System.out.println("Invalid task index");
         }
     }
 
-    public void deleteTask(int index) {
-        if (index >= 1 && index <= taskList.size()) {
-            Task removedTask = taskList.remove(index - 1); // Remove task at index
-            System.out.println("Noted. I've removed this task:");
-            System.out.println("  " + removedTask);
-            System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-        } else {
-            System.out.println("Invalid task index.");
-        }
+    // Method to load tasks from the file
+    private void loadTasks() {
+        storage.loadTasks(taskList);
+    }
+
+    // Method to save tasks to the file
+    private void saveTasks() {
+        storage.saveTasks(taskList);
     }
 }
 
