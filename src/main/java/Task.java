ca74f9040bb5ae6970a3f533d790597863e5b2f1
lakshm1i <e1155664@u.nus.edu>
2025-03-04 12:17:02 +0800

    Added JavaDoc comments to the code.
    Add header comments to the non-private classes/methods in all files

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,16 +1,37 @@
+/**
+ * Represents a task with a description and a completion status.
+ */
+
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a Task with the given description.
+     *
+     * @param description The description of the task.
+     */
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return "X" if the task is done, " " otherwise.
+     */
+
     public String getStatusIcon() {
         return (isDone) ? "X" : " ";
     }
+
+    /**
+     * Returns the description of the task.
+     *
+     * @return The task description.
+     */
 
     public String getDescription() {
         return description;
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,4 +1,15 @@
+/**
+ * Represents a task without a deadline or event time.
+ */
+
 public class ToDo extends Task {
+
+    /**
+     * Constructs a ToDo task with the given description.
+     *
+     * @param description The description of the task.
+     */
+
     public ToDo(String description) {
         super(description);
     }
